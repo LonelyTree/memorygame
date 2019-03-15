cff6fdbbec3f254465f5af2bf83403abaf65b08f
Alex Hughes <alexhugh121@gmail.com>
2019-03-14 21:04:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,22 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards= ["queen","queen","king","king"]
+
+var cardsInPlay=[]
+
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne)
+
+console.log("User flipped " + cardsInPlay[0])
+
+cardsInPlay.push(cardTwo)
+
+console.log("User flipped " + cardsInPlay[1])
+
+if(cardsInPlay.length === 2 && cardsInPlay[0].length === cardsInPlay[1].length){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.")
+}
