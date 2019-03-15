c3823424aac76d4edb1e549d948220c5895adc37
Alex Hughes <alexhugh121@gmail.com>
2019-03-15 15:37:55 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,19 +4,19 @@ var cards= ["queen","queen","king","king"]
 
 var cardsInPlay=[]
 
-var cardOne = cards[0]
-var cardTwo = cards[2]
+var checkForMatch = function() {
+if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	console.log("You found a match!");
+	} else if (cardsInPlay.length === 2) {
+	console.log("Sorry, try again.");}}
 
-cardsInPlay.push(cardOne)
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId] +"!");
+cardsInPlay.push(cards[cardId]);
+checkForMatch()}
 
-console.log("User flipped " + cardsInPlay[0])
 
-cardsInPlay.push(cardTwo)
+flipCard(0);
+flipCard(1);
 
-console.log("User flipped " + cardsInPlay[1])
 
-if(cardsInPlay.length === 2 && cardsInPlay[0].length === cardsInPlay[1].length){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.")
-}
